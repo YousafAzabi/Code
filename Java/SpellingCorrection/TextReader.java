d50bfdca84eec98645d639a41b061e490d0cc811
YousafAzabi <joeazabi@gmail.com>
2018-09-11 11:39:29 +0100

    add toLowerCase to checkIE call

--- Java/SpellingCorrection/TextReader.java
+++ Java/SpellingCorrection/TextReader.java
@@ -1,22 +1,22 @@
-class TextReader{
+class TextReader {
 
-	public static void main(String args[]) throws java.io.IOException{
+	public static void main(String args[]) throws java.io.IOException {
 
 		CIE cie = new CIE();
 		char ch;
 		String s = "", word = "", correct = "";
 
 		//loop to read a line from keyboard, one character at a time
-		while( (ch = (char) System.in.read()) != '\n'){
+		while ( (ch = (char) System.in.read()) != '\n') {
 			s = s + ch;
 		}
 		s = s + ' ';
 
-		for (int i=0 ; i < s.length() ; i++) //loop through line
-			if(s.charAt(i) != ' ') { //check space to strat new word
-				word = word + s.charAt(i); //constuct words
+		for (int k = 0; k < s.length(); k++) //loop through line
+			if (s.charAt(k) != ' ') { //check space to strat new word
+				word = word + s.charAt(k); //constuct words
 			} else { //
-				if (word.length() > 2){  //check length of word
+				if (word.length() > 2) { //check length of word
 					correct = cie.checkSpell(word);  //call spelling checker from CIE class
 					if (word.compareTo(correct) != 0) //compare word for misspelling
 						System.out.println("There is a misspelling in \'" + word + "\' correct spelling is: " + correct);
@@ -30,27 +30,27 @@ class TextReader{
 }
 
 //class to check misspelled words using rule "ie except c".
-class CIE{
+class CIE {
 //array of irrigular words
 private String[] irrigular = {"science","neighbour","species","sufficient","receive","ceiling","receipt","ceilidh"};
 
-public String checkSpell(String s){						//check against irrigular words
-	for(int i=0 ; i < irrigular.length ; i++){	//loop through array
-		if (s.equals(irrigular[i])) return s;			// if equal return
+public String checkSpell(String s) { //check against irrigular words
+	for (int k = 0; k < irrigular.length; k++) { //loop through array
+		if (s.equals(irrigular[k])) return s; // if equal return
 	}
-	return checkIE(s);													//call ie checker
+	return checkIE(s.toLowerCase()); //call ie checker
 }
 
-private String checkIE(String s){
-		for(int i=1 ; i < s.length() ; i++){
-			if ( (s.charAt(i) == 'i' | s.charAt(i) == 'I') & (s.charAt(i-1) == 'e' | s.charAt(i-1) == 'E')){
-				if( (s.charAt(i-2) != 'c' & s.charAt(i-2) != 'C')){
-					 s = s.replace("ei","ie");
+private String checkIE(String s) {
+		for (int k = 1; k < s.length(); k++) { //loop through characters in word
+			if ((s.charAt(k) == 'i') & (s.charAt(k-1) == 'e')) { //check 'ei'
+				if (s.charAt(k-2) != 'c') { //check if 'ei' not preceded by 'c'
+					 s = s.replace("ei","ie"); //relplace 'ei' with 'ie'
 				}
 			}
-			if ((s.charAt(i) == 'e' | s.charAt(i) == 'E') && (s.charAt(i-1) == 'i' | s.charAt(i-1) == 'I')){
-				if((s.charAt(i-2) == 'c' | s.charAt(i-2) == 'C')){
-					s = s.replace("ie","ei");
+			if ((s.charAt(k) == 'e') & (s.charAt(k-1) == 'i')) { //check 'ie'
+				if (s.charAt(k-2) == 'c') { //check if 'ei' is preceded by 'c'
+					s = s.replace("ie","ei"); //relplace 'ie' with 'ei'
 				}
 			}
 		}
