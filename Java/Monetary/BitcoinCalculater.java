5c32a4a9e42cdff5345a6a2609afde630e45488e
YousafAzabi <joeazabi@gmail.com>
2018-10-16 16:43:52 +0100

    changed Java/Monetray to receive input

--- Java/Monetary/BitcoinCalculater.java
+++ Java/Monetary/BitcoinCalculater.java
@@ -2,9 +2,7 @@ import java.util.ArrayList;
 
 class BitcoinCalculater {
 
-  public ArrayList<Integer> calculate(){
-    int[]  monetary = {20, 10, 5, 1};
-    int value = 97;
+  public ArrayList<Integer> calculate(int[] monetary, int value){
     ArrayList<Integer>  results = new ArrayList<Integer>();
     int i = 0;
     while ( i < monetary.length ){
@@ -27,15 +25,7 @@ class BitcoinCalculater {
 class TestBitcoinCalculater {
   public static void main(String args[]) {
     BitcoinCalculater bitcoin = new BitcoinCalculater();
-    System.out.println(bitcoin.calculate());
-    // ArrayList<Integer> arr = bitcoin.calculate();
-    // System.out.print("[ ");
-    // for (int i = 0; i < arr.length; i++){
-    //   System.out.print(arr[i]);
-    //   if(i < (arr.length - 1)) {
-    //     System.out.print(", ");
-    //   }
-    // }
-    // System.out.println("]");
+    int[] input = {20, 10, 5, 1};
+    System.out.println(bitcoin.calculate(input, 97));
   }
 }
